985abbc7c71d864fcd9f9fc891b59707ce18539b
lenovo <lenovo@LAPTOP-CPOHL3O3.zte.com.cn>
2018-01-28 14:49:13 +0300

    Update StreamRunner - add func interface and map method

--- TaskProj/src/StreamRunner.java
+++ TaskProj/src/StreamRunner.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +17,7 @@ public class StreamRunner {
 			}
 		};
 		Stream<StringBuilder> st = l.stream();
-		st.filter((p) -> p.toString().equals("1")).forEach((p) -> p.append("1"));//forEach - terminal operation!
+		st.filter((p) -> p.toString().equals("1")).forEach((p) -> p.append("1"));// forEach - terminal operation!
 		System.out.println(l);
 		List<StringBuilder> list = l.stream().filter((p) -> p.toString().equals("2") || new Integer(p.toString()) > 10)
 				.collect(Collectors.toList());
@@ -23,10 +25,21 @@ public class StreamRunner {
 		list = l.stream().sorted((o1, o2) -> new Integer(o1.toString().length()).compareTo(o2.toString().length()))
 				.collect(Collectors.toList());
 		System.out.println(list);
-		Optional<StringBuilder> max = l.stream().max((o1, o2) -> new Integer(o1.toString()).compareTo(new Integer(o2.toString())));
+		Optional<StringBuilder> max = l.stream()
+				.max((o1, o2) -> new Integer(o1.toString()).compareTo(new Integer(o2.toString())));
 		StringBuilder maxValue = max.orElse(new StringBuilder().append("Not found"));
 		System.out.println(maxValue.toString());
-		System.out.println(l.stream().peek((p) -> p.append("3")).collect(Collectors.toList()));//peek - not terminal operation!
-		System.out.println(String.join(" ", "Hello", "from", "Java", "8!"));
+		System.out.println(l.stream().peek((p) -> p.append("3")).collect(Collectors.toList()));// peek - not terminal
+																								// operation!
+		String[] arr = l.stream().map(o -> o.append("mapped")).map(StringBuilder::toString).toArray(String[]::new);
+		List<String> l1 = Arrays.asList(arr);
+		Collections.reverse(l1);
+		SomeI<String> func1 = o -> o + "-func#1";
+		l1.stream().forEach(o -> System.out.print(method(func1, o) + " "));
+		System.out.println(String.join(" ", "\nHello", "from", "Java", "8!"));
 	}
+
+	public static <T> T method(SomeI<T> func, T t) { return func.doSometh(t); }
 }
+
+interface SomeI<T> { T doSometh(T t);}
