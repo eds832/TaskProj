57fce89c809e0b6fa42c88c4883a26d7e5f2291a
lenovo <lenovo@LAPTOP-CPOHL3O3>
2019-02-17 00:12:05 +0300

    new

---
+++ TaskProj/src/ReverseString.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str = "I know Java";
+		StringBuilder sb = new StringBuilder(str);
+		System.out.println(sb.reverse().toString());
+		String[] strArr = str.split("");
+		String s = "";
+		for (int i = strArr.length - 1; i >= 0; i--) {
+			s += strArr[i];
+		}
+		System.out.println(s);
+		ArrayList<Character> charList = new ArrayList<>();
+		IntStream chars = str.chars();
+		chars.forEach(c -> charList.add(new Character((char) c)));
+		ListIterator<Character> it = charList.listIterator(charList.size());
+		s = "";
+		while (it.hasPrevious()) {
+			s += String.valueOf(it.previous());
+		}
+		System.out.println(s);
+		s = "";
+		for(int i = str.length() - 1; i >= 0; i--) {
+			s += String.valueOf(str.charAt(i));
+		}
+		System.out.println(s);
+		s = "";
+		charList.clear();
+		for(int i = str.length() - 1; i >= 0; i--) {
+			charList.add(str.charAt(i));
+		}
+		System.out.println(charList.stream().map(String::valueOf).collect(Collectors.joining("")));
+		s = "";
+		for(int i = str.length(); i > 0; i--) {
+			s += str.substring(i - 1, i);
+		}
+		System.out.println(s);
+	}
+
+}
