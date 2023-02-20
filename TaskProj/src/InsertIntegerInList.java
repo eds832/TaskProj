f09ce340bf840a1397c7561e90a3179fd9c221ca
eds832 <eds832@gmail.com>
2023-02-20 01:00:45 +0100

    Update InsertIntegerInList.java

--- TaskProj/src/InsertIntegerInList.java
+++ TaskProj/src/InsertIntegerInList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InsertIntegerInList {
@@ -22,6 +23,11 @@ public class InsertIntegerInList {
 		test.add(2);
 		test.add(3);
 		System.out.println(insertInt(test, 2, 8));
+		test = new LinkedList<Integer>();
+		test.add(1);
+		test.add(2);
+		test.add(3);
+		System.out.println(insertInt(test, 2, 8));
 		test = new ArrayList<Integer>();
 		test.add(1);
 		test.add(2);
@@ -32,14 +38,20 @@ public class InsertIntegerInList {
 		if(ints == null || position < 0 || position >= ints.size()) {
 			throw new IllegalArgumentException("array are null or position doesn't exist in array");
 		}
-		int valToReplace = ints.get(position);
-		ints.set(position, value);
-		for(int i = position + 1; i < ints.size(); i++) {
-			int nextValue = ints.get(i);
-			ints.set(i, valToReplace);
-			valToReplace = nextValue;
+		if(ints.getClass().getName().equals("java.util.LinkedList")) {
+			System.out.print("LinkedList");
+			ints.add(position, value);
+		} else {
+			System.out.print("ArrayList");
+			int valToReplace = ints.get(position);
+			ints.set(position, value);
+			for(int i = position + 1; i < ints.size(); i++) {
+				int nextValue = ints.get(i);
+				ints.set(i, valToReplace);
+				valToReplace = nextValue;
+			}
+			ints.add(valToReplace);
 		}
-		ints.add(valToReplace);
 		return ints;
 	}
 
